e34c9a071f3c1bead9465b4a6347e1fb1bcc4b68
GeorgeKimm <gosha.kim1998@mail.ru>
2021-05-14 14:43:25 +0600

    Revert "fix"
    This reverts commit 29ea00624197d9888b121b6cf85e63f32d73e929.

--- .eleventy.js
+++ .eleventy.js
@@ -46,6 +46,5 @@ module.exports = (config) => {
         htmlTemplateEngine: "njk",
         passthroughFileCopy: true,
         templateFormats: ["md", "njk"],
-        pathPrefix: "/kinoclan/",
     };
 };
